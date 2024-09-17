8b31bb7d87ef85998a57ca38c7d36a806051f7cf
KillianDB <killiandomingues57@gmail.com>
2024-09-17 10:21:09 -0300

    fix: setting user provider to app

--- src/App.tsx
+++ src/App.tsx
@@ -9,22 +9,28 @@ import Calculator from "./screens/Calculator";
 import { CreateUser } from "./screens/CreateUser";
 import { Equipes } from "./screens/Equipes";
 import ResultadoAnalise from "./screens/ResultadoAnalise";
+import { UserProvider } from "./utils/UserContext";
 // import { Loading } from "./screens/Loading";
 
 function App() {
 	return (
-		<Routes>
-			<Route path='/' element={<LoginScreen />} />
-			<Route path='/home' element={<Home />} />
-			<Route path='/admin/home' element={<AdminHome />} />
-			<Route path='/calculadora' element={<Calculator />} />
-			<Route path='/equipes' element={<Equipes />} />
-			{/* <Route path='/loading' element={<Loading />} /> */}
-			<Route path='/resultado-analise' element={<ResultadoAnalise />} />
-			{/* <Route path='*' element={<Calculator />} /> */}
-			<Route path='/criar-funcionario' element={<CreateUser />} />
-			{/* <Route path='*' element={<NotFoundScreen />}/> */}
-		</Routes>
+		<UserProvider>
+			<Routes>
+				<Route path='/' element={<LoginScreen />} />
+				<Route path='/home' element={<Home />} />
+				<Route path='/admin/home' element={<AdminHome />} />
+				<Route path='/calculadora' element={<Calculator />} />
+				<Route path='/equipes' element={<Equipes />} />
+				{/* <Route path='/loading' element={<Loading />} /> */}
+				<Route
+					path='/resultado-analise'
+					element={<ResultadoAnalise />}
+				/>
+				{/* <Route path='*' element={<Calculator />} /> */}
+				<Route path='/criar-funcionario' element={<CreateUser />} />
+				{/* <Route path='*' element={<NotFoundScreen />}/> */}
+			</Routes>
+		</UserProvider>
 	);
 }
 
